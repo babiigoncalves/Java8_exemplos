b9c0e7885ad36842e8e7657b8fc3b8b0d21e92c9
Barbara Goncalves <barbara.goncalves.nti@ufpe.br>
2016-07-13 11:33:22 -0300

    Testes do capítulo 9

--- src/br/com/exemplos/testes/Capitulo9.java
+++ src/br/com/exemplos/testes/Capitulo9.java
@@ -1,10 +1,33 @@
 package br.com.exemplos.testes;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class Capitulo9 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		String fileName = "./src/br/com/exemplos/testes";
+		Stream<String> strings = Files.list(Paths.get(fileName)).filter(p -> p.toString().endsWith("vos.java"))
+				.flatMap(p -> lines(p));
 
+		LongStream lines = Files.list(Paths.get(fileName)).filter(p -> p.toString().endsWith(".java"))
+				.mapToLong(p -> lines(p).count());
+		
+		lines.forEach(l->System.out.println(l));
+
+	}
+
+	static Stream<String> lines(Path p) {
+		try {
+			return Files.lines(p);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
 	}
 
 }
